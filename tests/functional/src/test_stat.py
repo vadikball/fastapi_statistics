7932df813ca7dd4b644dded24277249e1b641f51
vadik <etoseryozno@yandex.ru>
2022-11-30 22:20:02 +0300

    fixed tests

--- tests/functional/src/test_stat.py
+++ tests/functional/src/test_stat.py
@@ -6,51 +6,33 @@ from tests.functional.testdata.postgres_data import generate_data
 service_url = '/stats'
 
 
-@pytest.mark.parametrize(
-    'body, response',
-    [
-        (
-                {'success': True},
-                {'status': HTTPStatus.OK}
-        ),
-        (
-                {'success': False},
-                {'status': HTTPStatus.UNPROCESSABLE_ENTITY}
-        )
-    ]
-)
 @pytest.mark.asyncio
 async def test_stat(
         get_data,
         make_request,
         get_params,
-        body: dict,
-        response: dict
 ):
     data: list[dict] = get_data()
 
-    if body['success']:
-        for stat in data:
-            body, headers, status = await make_request('get', service_url, body=stat)
-            assert status == response['status']
-    else:
-        body, headers, status = await make_request('get', service_url, body=body)
-        assert status == response['status']
+    for stat in data:
+        body, headers, status = await make_request('get', service_url, body=stat)
+        assert status == HTTPStatus.OK
+
+    body, headers, status = await make_request('get', service_url, body={'success': False})
+    assert status == HTTPStatus.UNPROCESSABLE_ENTITY
 
     params = get_params(data)
     body, headers, status = await make_request('get', service_url, query_data=params)
-
     assert status == HTTPStatus.OK
+
     body_values = tuple(value for item in body['items'] for value in tuple(item.values()))
     assert data[0]['id'] in body_values
 
     body, headers, status = await make_request('delete', service_url)
-
     assert status == HTTPStatus.OK
 
     params = get_params(data)
     body, headers, status = await make_request('get', service_url, query_data=params)
-
     assert status == HTTPStatus.NOT_FOUND
 
 
